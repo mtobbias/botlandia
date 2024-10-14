49a4daf4035c8465ce47b2e60530565d7e20fa73
Marcelo Tobias <marcelo.tobbias@gmail.com>
2024-10-14 14:15:22 -0300

    fix: rabbit.util.ts

--- backend/src/tool-store/iara-tools/incarnation.tool.ts
+++ backend/src/tool-store/iara-tools/incarnation.tool.ts
@@ -17,7 +17,7 @@ export class IncarnationsTool extends Tool {
     constructor() {
         super(
             IncarnationsTool.UUID,
-            "Gerenciamento de Perfis de Agentes de Atendimento",
+            "acrd de Perfis de Agentes de Atendimento",
             `
             Ferramenta permite gerenciar perfis de agentes utilizados no atendimento via WhatsApp.
             Com esta ferramenta, você pode executar as seguintes ações:
